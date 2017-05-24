8f49c340983bb25e5d3bd7e18257e21640819612
Yasemin Varlik <varly065@newschool.edu>
2017-05-24 14:54:24 +0200

    chsnge sidebar

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,7 @@ var size = [window.width,window.height];
 $(window).resize(function(){
     window.resizeTo(size[0],size[1]);
 });
+
+$(".square").mouseover(function(){
+	$(".back").css("opacity",1);
+});
