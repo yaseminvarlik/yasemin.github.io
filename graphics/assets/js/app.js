9e61650128c402eef1b8bcc20d4bcbb0d6e9e3e8
Yasemin Varlik <varly065@newschool.edu>
2017-06-25 14:21:30 +0200

    add graphic design project

--- graphics/assets/js/app.js
+++ graphics/assets/js/app.js
@@ -4,3 +4,10 @@ var size = [window.width,window.height];
 $(window).resize(function(){
     window.resizeTo(size[0],size[1]);
 });
+
+var size = [window.width,window.height];
+
+
+$(".square").mouseover(function(){
+	$(".back").css("opacity",1);
+});
