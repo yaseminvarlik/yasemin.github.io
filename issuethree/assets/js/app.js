86c61e46d407e1bbae76fa44c6723f4be3852917
Yasemin Varlik <varly065@newschool.edu>
2017-12-11 15:26:23 -0500

    scroll roght function for issue three

--- issuethree/assets/js/app.js
+++ issuethree/assets/js/app.js
@@ -1,3 +1,19 @@
-$(".mainbody").click(function(){
-	console.log("hello");
+
+// $(document).ready(function () {
+//     $(".java").click(function () {
+//     	$(this).text('Scroll Right');
+//     });
+//     });
+
+$(document).ready(function () {
+    $('a').click(function () {
+    	$(this).css('text-decoration','none');
+    });
+
+ $('.java').click(function() {
+    var left = $(document).outerWidth() - $(window).width();
+	$('body, html').animate({ scrollLeft: '+=1100'}, 500);
+    console.log("hello");
 });
+
+    });
