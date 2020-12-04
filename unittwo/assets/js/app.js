e5cf0230d44b28cde5e241b7d0e85a8045acc003
Yasemin Varlik <varly065@newschool.edu>
2020-12-05 00:08:10 +0100

    new file

---
+++ unittwo/assets/js/app.js
@@ -0,0 +1,159 @@
+
+			$(document).ready(function() {
+
+
+			var applied = false;
+
+
+		 	$('.link_one').click(function(){
+		 		changeCSS('.one')
+		 	});
+		 	$('.link_two').click(function(){
+		 		changeCSS('.two')	
+		 	});
+		 	$('.link_three').click(function(){
+		 		changeCSS('.three')
+		 	});
+		 	$('.link_four').click(function(){
+		 		changeCSS('.four')
+		 	});
+		 	$('.link_five').click(function(){
+		 		changeCSS('.five')
+		 	});
+		 	$('.link_six').click(function(){
+		 		changeCSS('.six')
+		 	});
+		 	$('.link_seven').click(function(){
+		 		changeCSS('.seven')
+		 	});
+		 	$('.link_eight').click(function(){
+		 		changeCSS('.eight')
+		 	});
+		 	$('.link_nine').click(function(){
+		 		changeCSS('.nine')
+		 	});
+		 	$('.link_ten').click(function(){
+		 		changeCSS('.ten')
+		 	});
+		 	$('.link_eleven').click(function(){
+		 		changeCSS('.eleven')
+		 	});
+		 	$('.link_twelve').click(function(){
+		 		changeCSS('.twelve')
+		 	});
+		 	$('.link_thirteen').click(function(){
+		 		changeCSS('.thirteen')
+		 	});
+		 	$('.link_fourteen').click(function(){
+		 		changeCSS('.fourteen')
+		 	});
+		 	$('.more').click(function(){
+		 		changeCSS('.issue_two')
+		 	});
+		 	$('.more_two').click(function(){
+		 		changeCSS('.issue_three')
+		 	});
+
+			function changeCSS(el) {
+				if (!applied) {
+				$(el).css({"display": "block"
+						});
+				applied = true;
+
+				} else {
+				$(el).css("display", "none");
+				applied = false;
+				};
+			}
+			$('.link_one').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_one').offset().top},'slow');
+			
+		});
+			$('.more').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.more').offset().top},'slow');
+			
+		});
+			$('.more_two').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.more_two').offset().top},'slow');
+			
+		});
+			$('.link_two').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_two').offset().top},'slow');
+			
+		});
+			$('.link_three').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_three').offset().top},'slow');
+			
+		});
+			$('.link_four').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_four').offset().top},'slow');
+			
+		});
+			$('.link_five').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_five').offset().top},'slow');
+			
+		});
+			$('.link_eight').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_eight').offset().top},'slow');
+			
+		});
+			$('.link_six').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_six').offset().top},'slow');
+			
+		});
+			$('.link_seven').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_seven').offset().top},'slow');
+			
+		});
+			$('.link_nine').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_nine').offset().top},'slow');
+			
+		});
+			$('.link_ten').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_ten').offset().top},'slow');
+			
+		});
+			$('.link_eleven').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_eleven').offset().top},'slow');
+			
+		});
+			$('.link_twelve').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_twelve').offset().top},'slow');
+			
+		});
+			$('.link_thirteen').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_thirteen').offset().top},'slow');
+			
+		});
+			$('.link_fourteen').click(function(){
+				$('html,body').animate({
+        		scrollTop: $('.link_fourteen').offset().top},'slow');
+			
+		});
+});
+			
+
+	
+   		 
+  
+
+
+
+
+
+			
